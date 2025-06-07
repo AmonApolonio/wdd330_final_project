730bfe8329756223077492b1c5acff5d88e46180
Amon Apolonio <amonapolonio.dev@gmail.com>
2025-06-07 20:23:00 -0300

    fix: deploy fix test

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+  base: "/wdd330_final_project/",
 
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        apiTest: resolve(__dirname, "src/api-test.html"),
       },
     },
   },
